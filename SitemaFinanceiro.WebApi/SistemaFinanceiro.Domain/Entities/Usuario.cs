58ba603e4ea5a5ba0d437c4c35ee4a53051dfa68
Matheus Henrique Monfreides <85165323+MatheusMonfreides@users.noreply.github.com>
2023-11-07 10:19:57 -0300

    Entitie Usuario

---
+++ SitemaFinanceiro.WebApi/SistemaFinanceiro.Domain/Entities/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFinanceiro.Domain.Entities
+{
+    public class Usuario
+    {
+        public string FullName { get; set; }
+        public string Avatar { get; set; }
+        //public StatusUsuarioEnum Status { get; set; } = StatusUsuarioEnum.Ativo;
+
+    }
+}
